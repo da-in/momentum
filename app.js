e2775f6eb8e78acd3e9408eddec9e4ca100a4cb1
unknown <talown@naver.com>
2021-07-05 19:31:42 +0900

    boolean null undefined

--- app.js
+++ app.js
@@ -1,12 +1,4 @@
-let a = 5;
-let b = 2;
-let myName = "dain";
-
-console.log(a+b);
-console.log(a*b);
-console.log(a/b);
-console.log("hello " + myName)
-
-myName = "choi dain"
-
-console.log("your new name is " + myName)
+const amIFat = null;
+let something;
+console.log(amIFat);
+console.log(something);
