3ed7395ddf974414477e18643e2bd641bb543e2b
unknown <talown@naver.com>
2021-07-05 19:07:04 +0900

    const let var

---
+++ app.js
@@ -0,0 +1,12 @@
+let a = 5;
+let b = 2;
+let myName = "dain";
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("hello " + myName)
+
+myName = "choi dain"
+
+console.log("your new name is " + myName)
