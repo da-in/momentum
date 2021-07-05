11a5443f8dc4e6638efc81578de6e4cedffdf75f
unknown <talown@naver.com>
2021-07-05 20:23:32 +0900

    array

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-const amIFat = null;
-let something;
-console.log(amIFat);
-console.log(something);
+const mon='mon'
+const tue='tue'
+const wed='wed'
+const thu='thu'
+const fri='fri'
+const sat='sat'
+const sun='sun'
+
+const daysOfWeek = [mon, tue, wed, thu, fri, sat, sun];
+console.log(daysOfWeek);
